c50c449df81e960bf003a455848919744d535a3a
Federicotalmon <54511081+Federicotalmon@users.noreply.github.com>
2022-05-04 00:26:39 -0300

    reparacion de error

--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Medico;
+use App\Models\medico;
 use App\Models\Obra_social;
 
 class MedicoController extends Controller
